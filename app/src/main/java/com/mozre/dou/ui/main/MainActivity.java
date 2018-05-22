ca6429cd922f88cdd839ffe2a6e4d95123cc051b
mozre <cdxy_zhanjing@163.com>
2018-05-23 07:36:30 +0800

    [Requirement]add mvp constructor folders 	modified:   app/build.gradle 	modified:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/mozre/dou/ui/main/MainActivity.java 	new file:   app/src/main/res/layout/activity_main.xml

---
+++ app/src/main/java/com/mozre/dou/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mozre.dou.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mozre.dou.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
